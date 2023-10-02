178d9ba186d323aafd75d3415a8f0c20b5001182
lululwtv <85116693+lululwtv@users.noreply.github.com>
2023-10-03 01:06:51 +0800

    Revert "test"
    This reverts commit 9a4ac50900cb015ea0855024eb5fec4c8b460664.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package seedu.address.logic.parser;
 
 /**
@@ -14,20 +13,3 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
 }
-=======
-package seedu.address.logic.parser;
-
-/**
- * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
- */
-public class CliSyntax {
-
-    /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-
-}
->>>>>>> parent of 440a0682 (AddressBook: Add 'remarks' command for enhanced note-taking)
