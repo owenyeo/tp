1bc6553da4ec21b4ad6cadffb8cb9c77369e697d
lululwtv <85116693+lululwtv@users.noreply.github.com>
2023-10-15 12:17:08 +0800

    Fix bugs with MealBuddyCommand

--- src/main/java/seedu/address/logic/commands/MealBuddyCommand.java
+++ src/main/java/seedu/address/logic/commands/MealBuddyCommand.java
@@ -1,7 +1,10 @@
 package seedu.address.logic.commands;
 
+import java.util.ArrayList;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Checks for a contact with the same free time as the User and returns a list of contacts with the same free time.
