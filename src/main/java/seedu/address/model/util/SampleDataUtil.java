c70b3802cceb3f6fd3aa31611f995f90d2a7a606
lululwtv <85116693+lululwtv@users.noreply.github.com>
2023-11-02 01:02:55 +0800

    Remove 5 methods from SampleDataUtil
    Remove getSampleCca()
    
    Remove getSampleModule()
    
    Remove getSampleMeetUpEvents()
    
    Remove getSampleDatedEvents()
    
    Remove getSampleSchedule()

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -58,43 +58,6 @@ public class SampleDataUtil {
         };
     }
 
-    public static List<Module> getSampleModules() {
-        List<Module> modules = new ArrayList<>();
-        modules.add(new Module("CS2103", "Wednesday 1200 1300"));
-        modules.add(new Module("CS2101", "Tuesday 1200 1400"));
-        modules.add(new Module("CS2100", "Monday 1400 1500"));
-        return modules;
-    }
-
-    public static List<Cca> getSampleCcas() {
-        List<Cca> ccas = new ArrayList<>();
-        ccas.add(new Cca("Basketball", "Monday 1800 2000"));
-        ccas.add(new Cca("Soccer", "Tuesday 1800 2000"));
-        ccas.add(new Cca("Tennis", "Wednesday 1800 2000"));
-        return ccas;
-    }
-
-    public static ArrayList<DatedEvent> getSampleDatedEvents() {
-        ArrayList<DatedEvent> datedEvents = new ArrayList<>();
-        datedEvents.add(DatedEvent.newDatedEvent("CS2103 Meeting 2023-10-10 1030 1130 y"));
-        datedEvents.add(DatedEvent.newDatedEvent("Walk Dog 2023-10-10 1030 1130 n"));
-        datedEvents.add(DatedEvent.newDatedEvent("Competitive sleeping 2023-10-10 1030 1130 y"));
-        return datedEvents;
-    }
-
-    public static List<MeetUpEvent> getSampleMeetUpEvents() {
-        List<MeetUpEvent> meetUpEvents = new ArrayList<>();
-        meetUpEvents.add(MeetUpEvent.newMeetUpEvent("CS2103 Meeting 2023-10-10 1030 1130 y", getSamplePersons()[0]));
-        meetUpEvents.add(MeetUpEvent.newMeetUpEvent("Walk Dog 2023-10-10 1030 1130 n", getSamplePersons()[1]));
-        meetUpEvents.add(MeetUpEvent.newMeetUpEvent("Competitive sleeping 2023-10-10 1030 1130 y",
-                getSamplePersons()[2]));
-        return meetUpEvents;
-    }
-
-    public static Schedule getSampleSchedule() {
-        return new Schedule(getSampleModules(), getSampleCcas(), getSampleDatedEvents(), getSampleMeetUpEvents());
-    }
-
     public static User getSampleUser() {
         return new User(new Name("Me"), new Phone("00000000"), new Email("me@example.com"),
             new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Birthday("2000-01-01"),
