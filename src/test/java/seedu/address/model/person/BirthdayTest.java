a2f359a74dc567d7c80bb0a7355f2bb35875a730
Teo Zheng Yang <teozhengyang15@gmail.com>
2023-11-02 03:49:36 +0800

    Add testcases for AddEventCommand. AddScheduleCommand. RemoveEventCommand, RemoveEventCommandTest

--- src/test/java/seedu/address/model/person/BirthdayTest.java
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -1,4 +1,22 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
 public class BirthdayTest {
+    @Test
+    public void equals() {
+        Birthday birthday = new Birthday("2001-04-19");
+        assertFalse(birthday.equals(null));
+    }
+
+    @Test
+    public void getDate() {
+        Birthday birthday = new Birthday("2001-04-19");
+        assertTrue(birthday.getDate().equals(LocalDate.parse("2001-04-19")));
+    }
 }
