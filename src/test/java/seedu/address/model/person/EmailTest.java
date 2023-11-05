c37c089ae3b0aedd3eb0192db7bc43dcc7cec6ce
Teo Zheng Yang <teozhengyang15@gmail.com>
2023-11-06 00:02:04 +0800

    Fix test cases error in models

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -15,19 +15,9 @@ public class EmailTest {
     }
 
     @Test
-    public void constructor_invalidEmail_throwsIllegalArgumentException() {
-        String invalidEmail = "";
-        assertThrows(IllegalArgumentException.class, () -> new Email(invalidEmail));
-    }
-
-    @Test
     public void isValidEmail() {
         // null email
         assertThrows(NullPointerException.class, () -> Email.isValidEmail(null));
-
-        // blank email
-        assertFalse(Email.isValidEmail("")); // empty string
-        assertFalse(Email.isValidEmail(" ")); // spaces only
 
         // missing parts
         assertFalse(Email.isValidEmail("@example.com")); // missing local part
