cddb48820f60b54d6cfa38dc9a585dc4d1eec144
Teo Zheng Yang <teozhengyang15@gmail.com>
2023-11-08 16:19:45 +0800

    Fix bugs in test cases for logic classes

--- src/test/java/seedu/address/logic/parser/RemoveEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemoveEventCommandParserTest.java
@@ -1,13 +1,11 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENTNAME;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.RemoveEventCommand;
 
 public class RemoveEventCommandParserTest {
@@ -18,11 +16,11 @@ public class RemoveEventCommandParserTest {
 
         // dated event in user
         assertParseSuccess(parser, "user en/CS2103 Meeting",
-                new RemoveEventCommand("cs2103 meeting", null));
+                new RemoveEventCommand("CS2103 MEETING", null));
 
         // dated event in friend
         assertParseSuccess(parser, "1 en/CS2103 Meeting",
-                new RemoveEventCommand("cs2103 meeting", Index.fromOneBased(1)));
+                new RemoveEventCommand("CS2103 MEETING", Index.fromOneBased(1)));
     }
 
     @Test
@@ -31,39 +29,25 @@ public class RemoveEventCommandParserTest {
 
         // multiple event name
         assertParseFailure(parser, validCommand + " en/CS2103 Meeting",
-                Messages.getErrorMessageForDuplicatePrefixes(PREFIX_EVENTNAME));
+                "You can only have 1 of each prefix!\n" + "Duplicated prefixes are: en/ ");
 
-        // multiple fields repeated
-        assertParseFailure(parser, validCommand + " en/CS2103 Meeting",
-                Messages.getErrorMessageForDuplicatePrefixes(PREFIX_EVENTNAME));
-
-
-        //valid value followed by invalid value
-
-        // invalid name
-        assertParseFailure(parser, validCommand + " en/CS2103 Meeting",
-                Messages.getErrorMessageForDuplicatePrefixes(PREFIX_EVENTNAME));
     }
 
     @Test
     public void parse_fieldMissing_failure() {
-        String expectedMessage = String.format("Command format is invalid! \n"
-                + RemoveEventCommand.MESSAGE_USAGE);
-
-        String expectedMessage2 = String.format("Invalid index!\n"
-                + "Index can only be 'user' or a positive integer! \n");
-
         // missing event name prefix
         assertParseFailure(parser, "user",
-                expectedMessage);
+                "Missing prefix(es) for en/ !\n" + "Message Usage:\n" + RemoveEventCommand.MESSAGE_USAGE);
 
         // all prefixes missing
         assertParseFailure(parser, "user CS2103 Meeting",
-                expectedMessage);
+                "Missing prefix(es) for en/ !\n" + "Message Usage:\n"
+                        + RemoveEventCommand.MESSAGE_USAGE);
 
         //wrong index
         assertParseFailure(parser, "wrong en/CS2103 Meeting",
-                expectedMessage2);
+                String.format("Invalid index!\n"
+                        + "Index can only be 'user' or a positive integer! \n"));
     }
 
 }
