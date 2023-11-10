65b4e68f77acc419e569ff4f63e5496b8bddddf9
owenyeo <121847473+owenyeo@users.noreply.github.com>
2023-11-10 10:26:00 +0800

    Fix Add Command
    Add command does not throw an error message when
    prefixes other than name is not present.
    
    Fixed add command, added other prefixes in arePrefixesPresent
    clause.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -39,7 +39,8 @@ public class AddCommandParser implements Parser<AddCommand> {
                         PREFIX_BIRTHDAY, PREFIX_TAG);
 
         // Check if all prefixes are present
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_BIRTHDAY, PREFIX_TAG)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
